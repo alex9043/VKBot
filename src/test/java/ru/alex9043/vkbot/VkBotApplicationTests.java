1507b0b67379bc4644238545a6a55e6e045e856a
alex9043 <alex980127@gmail.com>
2025-04-13 13:45:02 +0300

    Init project

---
+++ src/test/java/ru/alex9043/vkbot/VkBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex9043.vkbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
